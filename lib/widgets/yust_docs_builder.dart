f01b69d4fb0cf1acf446487975658477cc7fc44a
Eyk Jacobsen <eja@traser.intern>
2019-08-29 14:20:52 +0200

    Added limit in docs_builder

--- lib/widgets/yust_docs_builder.dart
+++ lib/widgets/yust_docs_builder.dart
@@ -5,19 +5,26 @@ import '../models/yust_doc_setup.dart';
 import '../yust.dart';
 
 class YustDocsBuilder<T extends YustDoc> extends StatelessWidget {
-  
   final YustDocSetup modelSetup;
   final List<List<dynamic>> filter;
   final List<String> orderBy;
   final bool doNotWait;
   final Widget Function(List<T>) builder;
+  final num limit;
 
-  YustDocsBuilder({@required this.modelSetup, this.filter, this.orderBy, this.doNotWait = false, @required this.builder});
-  
+  YustDocsBuilder(
+      {@required this.modelSetup,
+      this.filter,
+      this.orderBy,
+      this.doNotWait = false,
+      @required this.builder,
+      this.limit});
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<List<T>>(
-      stream: Yust.service.getDocs<T>(modelSetup, filterList: filter, orderByList: orderBy),
+      stream: Yust.service.getDocs<T>(modelSetup,
+          filterList: filter, orderByList: orderBy, limit: limit),
       builder: (context, snapshot) {
         if (snapshot.hasError) {
           throw snapshot.error;
@@ -29,5 +36,4 @@ class YustDocsBuilder<T extends YustDoc> extends StatelessWidget {
       },
     );
   }
-
 }
